5c5f0c44bfc91b79b261f60f47ee3ee380546245
Brian Bug <thebrianbug@gmail.com>
2025-02-27 20:47:27 -0600

    Fix playwright

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -1,15 +1,10 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run build && npm run preview',
     port: 3000,
     reuseExistingServer: !process.env.CI,
-    cwd: __dirname
+    cwd: process.cwd()
   },
   testDir: 'tests',
   testMatch: /(.+\.)?(test|spec)\.[jt]s/
