aa3e7d123b98968841376a07f1a7ff86564be5bb
Brian Bug <thebrianbug@gmail.com>
2025-02-28 11:36:32 -0600

    Format

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { svelteTesting } from '@testing-library/svelte/vite'
+import { svelteTesting } from '@testing-library/svelte/vite';
 
 export default defineConfig({
   plugins: [svelte(), svelteTesting()],
