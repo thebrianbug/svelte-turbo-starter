d59651657044525ec2cf320a4f2b7610d60fcadb
Brian Bug <thebrianbug@gmail.com>
2025-03-01 14:19:25 -0600

    Simplify

--- packages/db/src/config/database.ts
+++ packages/db/src/config/database.ts
@@ -1,4 +1,11 @@
-export const databaseConfig = {
+export interface DatabaseConfig {
+  url: string;
+  pool: {
+    max: number;
+  };
+}
+
+export const databaseConfig: DatabaseConfig = {
   get url() {
     return (
       process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/svelte_turbo_db'
