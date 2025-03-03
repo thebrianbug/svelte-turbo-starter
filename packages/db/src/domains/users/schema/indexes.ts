6a4b7cb81fefa7ece52ca61dd0d798b03526f944
Brian Bug <thebrianbug@gmail.com>
2025-03-03 14:36:24 -0600

    Remove redundant index

--- packages/db/src/domains/users/schema/indexes.ts
+++ packages/db/src/domains/users/schema/indexes.ts
@@ -1,7 +1,2 @@
-import { index } from 'drizzle-orm/pg-core';
-
-import { users } from './schema';
-
-export const userIndexes = {
-  usersEmailIdx: index('users_email_idx').on(users.email)
-};
+// No need for additional indexes since UNIQUE constraints automatically create indexes
+export const userIndexes = {};
