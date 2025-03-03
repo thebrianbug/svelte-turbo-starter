e7735f8626d392e3b173e6fe6ad3027b6a67f76c
Brian Bug <thebrianbug@gmail.com>
2025-03-03 14:37:32 -0600

    Remove indicie file

--- packages/db/src/domains/users/schema/index.ts
+++ packages/db/src/domains/users/schema/index.ts
@@ -1,5 +1,4 @@
 export * from './schema';
-export * from './indexes';
 
 // Re-export the default export from schema
 export { default as users } from './schema';
--- packages/db/src/domains/users/schema/indexes.ts
+++
@@ -1,2 +0,0 @@
-// No need for additional indexes since UNIQUE constraints automatically create indexes
-export const userIndexes = {};
