5a39bcf2679aafc9aa7ada4eb97d49d17c68c06f
Brian Bug <thebrianbug@gmail.com>
2025-03-06 18:26:41 -0600

    Simplify exports

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,24 +1,8 @@
-// Database types and schema
-export type { User, NewUser, UserStatus } from './src/domains/users/models/user';
-export { users, userStatusEnum } from './src/domains/users/schema';
-
-// Repository exports
-export type { IUserRepository } from './src/domains/users/interfaces/i-user-repository';
-import { UserRepository } from './src/domains/users/infrastructure/user-repository';
+export * from './src/domains/users';
 
 // Pre-built repository instance
+import { UserRepository } from './src/domains/users';
 export const userRepository = new UserRepository();
-
-// Validation utilities
-export {
-  validateNewUser,
-  validateUpdateUser,
-  validateManyNewUsers,
-  newUserSchema,
-  updateUserSchema,
-  type ValidatedNewUser,
-  type ValidatedUpdateUser
-} from './src/domains/users/models/user';
 
 // Database utilities
 export { checkDatabaseConnection } from './src/database';
---
+++ packages/db/src/domains/users/index.ts
@@ -0,0 +1,10 @@
+// Models and types
+export type { User, NewUser, UserStatus, ValidatedNewUser, ValidatedUpdateUser } from './models/user';
+export { validateNewUser, validateUpdateUser, validateManyNewUsers, newUserSchema, updateUserSchema } from './models/user';
+
+// Schema
+export { users, userStatusEnum } from './schema';
+
+// Repository
+export type { IUserRepository } from './interfaces/i-user-repository';
+export { UserRepository } from './infrastructure/user-repository';
