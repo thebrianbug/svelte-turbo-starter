36337aa35b85bcda63033cfdeba309a8937e0b43
Brian Bug <thebrianbug@gmail.com>
2025-03-06 18:27:24 -0600

    Rearranged export

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,8 +1,9 @@
+// Database utilities
+export { checkDatabaseConnection } from './src/database';
+
+// Domain models
 export * from './src/domains/users';
 
 // Pre-built repository instance
 import { UserRepository } from './src/domains/users';
 export const userRepository = new UserRepository();
-
-// Database utilities
-export { checkDatabaseConnection } from './src/database';
