676cf688b71b499e06e2993913546cb5006a5771
Brian Bug <thebrianbug@gmail.com>
2025-03-07 18:11:07 -0600

    make unit tests explicit

---
+++ packages/bll/vitest.unit.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['src/**/*.test.ts'],
+    exclude: ['src/**/*.integration.test.ts'],
+    reporters: ['default'],
+    silent: true
+  }
+});
