492b6dabaa791f397ddc87e45b14515d8a76cc9e
Brian Bug <thebrianbug@gmail.com>
2025-03-11 13:44:56 -0500

    Export test utils

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -6,3 +6,6 @@ export * from './src/domains/users';
 
 // Repository factories
 export { createUserRepository } from './src/domains/users';
+
+// Test utilities - only imported by BLL integration tests
+export * as TestUtils from './tests';
---
+++ packages/db/tests/index.ts
@@ -0,0 +1,5 @@
+// Export test utilities for BLL layer integration tests
+export * from './integration/test-utils/database';
+export * from './integration/test-utils/test-assertions';
+export * from './integration/test-utils/repository-factories';
+export * from './integration/test-utils/database-migrations';
